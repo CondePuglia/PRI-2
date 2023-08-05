367bf166fe8a32e7d9cc4b046cc9abb2c11db4e2
ricardo <rienzopc@gmail.com>
2023-08-05 19:06:39 -0300

    tela-loginV1

---
+++ login.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AmbSin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo.css">
+<style>
+    .p{ 
+        border-radius: 5vh;
+        font-size: 1.2em;
+        padding: 5vh 1vh;
+        margin-top:20vh;
+    }
+
+
+   
+</style>
+
+
+</head>
+<body class="bg-dark">
+
+<span class="row bg-black text-emphasis-primary">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item "><a href="#" class="nav-link active bg-warning text-dark" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-warning">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-warning">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-warning">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-warning">About</a></li>
+      </ul>
+    </header>
+</span>
+<span class="row">
+    <span class="col-3">
+        </span>
+    <span class="col-3"></span>
+    <span class="col-3 bg-dark text-white border border-warning border-5 p ">
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" class="btn btn-warning">Submit</button>
+        </form>
+    </span>
+    <span class="col-3"></span>
+</span>
+
+
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</html>
