974566049156fed818874d8fd685d15430c519db
Rafael Soares <134234256+AugustoSRa@users.noreply.github.com>
2023-08-21 10:04:18 -0300

    Add files via upload

---
+++ PRI - Cópia/telas/inicio/inicio.php
@@ -0,0 +1,21 @@
+<?php
+
+require "../header.php";
+
+require "../menu.php";
+?>
+<div class="container" id="inicio">
+    <section class="section-1">
+
+    <img src="../login/img/undraw_access_account_re_8spm.svg" alt="">
+
+    
+    </section>
+
+
+</div>
+
+
+<?php
+require "../footer.php"
+?>
